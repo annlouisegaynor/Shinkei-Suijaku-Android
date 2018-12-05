bd3db8f37ce0d5ecbb6f0e4228bfb61927373370
alkgaynor <annlouisegaynor@gmail.com>
2018-12-05 19:20:12 +0800

    Fixed remaning bugs. Added countdown timer function.

--- app/src/main/java/com/example/akgaynor/activity04/GameActivity.java
+++ app/src/main/java/com/example/akgaynor/activity04/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.akgaynor.activity04;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,38 +11,97 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 public class GameActivity extends AppCompatActivity {
 
+    // Arrays
     private static Button cards[] = new Button[16];
-    private ArrayList<Integer> iconList = new ArrayList<Integer>();
-    private static boolean endgame;
-
+    private ArrayList<Integer> iconList = new ArrayList<>();
+    // Counters
     private int hits = 0;
     private int misses = 0;
-    private int counter = 1;
-
+    private int counter = 0;
+    // Trackers
     private int[] id = new int[2];
     private int[] value = new int[2];
-
+    // TextViews
     private TextView hitTextView;
     private TextView missTextView;
+    private TextView cdTextView;
+    // Booleans
+    private boolean endgame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // MAIN FUNCTION fired on activity startup
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        initTextViews();
-        initCards();
+        // Initialize elements
         initArrayList();
+        initCards();
+        initTextViews();
+
+        // Begin countdown of 3 seconds
+        countdown();
     }
 
-    public void initTextViews(){
-        hitTextView = findViewById(R.id.hitTextView);
-        missTextView = findViewById(R.id.missTextView);
+
+    public void initArrayList(){
+        /* INITIALIZE ARRAYLIST with 16 shuffled numbers
+
+            iconList is an arraylist that contains the values for the
+            16 cards. The number of cards divided by 2 (16/2 = 8) will
+            be the number of pairs (8). Numbers 1 - 8 exist twice in
+            this arraylist, totaling 16 elements.
+
+            The list is shuffled to have the placement of cards
+            randomized each game .
+         */
+        for (int y = 0; y < (cards.length/2); y++) {
+            iconList.add(y);
+            iconList.add(y);
+        }
+        Collections.shuffle(iconList);
+
+        /*
+            CHEATS for debugging purposes
+
+            The code below prints out the answers to the puzzle in the
+            in the log console. It shows the placement of the values
+            behind he cards.
+         */
+
+        int character = 0;
+        String line = "";
+
+        for (int a = 0; a < iconList.size(); a++) {
+            line += iconList.get(a) + " ";
+            character++;
+            if (character == 4) {
+                line += "\n";
+                character = 0;
+            }
+        }
+        Log.d("Cheats", "\n" +line);
     }
+
+
     public void initCards(){
+        /*  INITIALIZE CARDS with button references
+        
+            There are 16 cards laid out in a 4x4 grid.
+            Each card references a button element in the layout
+            
+            Get the card's referenced button using the button's ID and 
+            store them in their respective array indexes for later 
+            manipulation. Display their respective values until
+            countdown to 0 from 3 is complete.
+         */
+        
         cards[0] = (Button)findViewById(R.id.button1);
         cards[1] = (Button)findViewById(R.id.button2);
         cards[2] = (Button)findViewById(R.id.button3);
@@ -58,31 +118,69 @@ public class GameActivity extends AppCompatActivity {
         cards[13] = (Button)findViewById(R.id.button14);
         cards[14] = (Button)findViewById(R.id.button15);
         cards[15] = (Button)findViewById(R.id.button16);
+
+        for(int x = 0; x < cards.length; x++){
+            cards[x].setText(iconList.get(x) + "");
+        }
     }
 
-    public void initArrayList(){
-        for (int y = 0; y < (cards.length/2); y++) {
-            iconList.add(y);
-            iconList.add(y);
-        }
-        Collections.shuffle(iconList);
 
-        //  CHEATS HAHAHAHAHAHAHAHA
-        int character = 0;
-        String line = "";
+    public void initTextViews(){
+        /*  INITIALIZE TEXTVIEWS with TextView references
 
-        for (int a = 0; a < iconList.size(); a++) {
-            line += iconList.get(a) + " ";
-            character++;
-            if (character == 4) {
-                line += "\n";
-                character = 0;
+            There are 3 TextViews, one for displaying the # of hits
+            one for the # of misses, and one for displaying the
+            countdown to 0.
+
+            Find those TextViews based on their IDs and store them in
+            their respective variables for later manipulation
+
+            hits - number of pairs correctly matched
+            misses - number of pairs incorrectly matched
+            cd - countdown
+         */
+
+        hitTextView = (TextView)findViewById(R.id.hitTextView);
+        missTextView = (TextView)findViewById(R.id.missTextView);
+        cdTextView = (TextView)findViewById(R.id.cdTextView);
+    }
+
+
+    public void countdown(){
+        /*
+            COUNTDOWN 3 seconds
+
+            Revert the values of the cards back to the default, ???,
+            after 3 seconds.
+
+         */
+
+        new CountDownTimer(3000, 1000){
+            public void onTick(long millisUntilFinished){
+                millisUntilFinished = TimeUnit.MILLISECONDS.toSeconds(
+                        millisUntilFinished);
+                cdTextView.setText(millisUntilFinished + "");
             }
-        }
-        Log.d("cheats b0i", "\n" +line);
+            public void onFinish(){
+                for(int x = 0; x < cards.length; x++){
+                    cards[x].setText(R.string.cards_default);
+                }
+                cdTextView.setText(R.string.default_zero);
+            }
+        }.start();
     }
 
+    
     public boolean sameValues() {
+        /*  SAME VALUES?
+        
+            Checks if pair of cards have matching values. If they do,
+            add to the number of hits and display accordingly. If they
+            do not add to the number of misses and display accordingly.
+        
+            value[0] and value [1] are defined in onCardClick()
+         */
+        
         if (value[0] == value[1]) {
             hits++;
             hitTextView.setText(hits +"");
@@ -95,7 +193,16 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    public static boolean isEndGame(){
+    
+    public boolean isEndGame(){
+        /* IS ENDGAME?
+            
+            Checks if all the matching pairs have been found. If yes,
+            return true, if no, return false.
+            
+            Function is used in onCardClick();
+         */
+        
         for(int i=0; i < cards.length; i++){
             if( cards[i].isEnabled() == false){
                 endgame = true;
@@ -105,11 +212,51 @@ public class GameActivity extends AppCompatActivity {
         return true;
     }
 
+    
     public void onCardClick(View v){
+        /*
+            ON CARD CLICK EVENT HANDLER
+            
+            When a card is tapped, iterate through Button array "cards".
+            If the button tapped matches a button in the array, display
+            the tapped card's corresponding value and then increment the
+            counter.
+
+            id[] and value[] are trackers that help keep track of the
+            cards matched with each other.
+
+            If the counter is 1, store the value of the counter i in id[0]
+            and the value of the tapped card in value[0]. Likewise for
+            counter 2.
+            
+            If the counter is 2, two cards have been tapped. Check if they
+            have the same value through the sameValues() function. If they
+            match, disable the buttons so that the user will not be able
+            to tap them again and bring back the counter to 0. If they do
+            not match, on the next card tapped, the counter will become 3,
+            satisfying the counter == 3 condition.
+
+            If the counter is 3, meaning the user has incorrectly matched
+            two cards and has picked another one. Set the mismatched cards
+            back to their default value of ??? and bring back the counter
+            to 1 so that the next card tapped will match the previous one.
+
+            Repeat until isEndGame variable is true. When endgame has been
+            reached, display via Toast that the user is done with the
+            puzzle.
+            
+         */
+        
         for (int i = 0; i < cards.length; i++) {
             if (cards[i].getId() == v.getId()) {
                 cards[i].setText(iconList.get(i) +"");
+                counter++;
 
+                if (counter == 3) {
+                    cards[id[0]].setText(R.string.cards_default);
+                    cards[id[1]].setText(R.string.cards_default);
+                    counter = 1;
+                }
                 if (counter == 1) {
                     id[0] = i;
                     value[0] = iconList.get(i);
@@ -124,17 +271,12 @@ public class GameActivity extends AppCompatActivity {
                         counter = 0;
                     }
                 }
-
-                if (counter == 3){
-                    cards[id[0]].setText(R.string.a02_cards_default);
-                    cards[id[1]].setText(R.string.a02_cards_default);
-                    counter = 0;
-                }
-                counter++;
+                // Log.d("counter", counter +""); For debugging purposes
             }
         }
         if (isEndGame() == true){
-            Toast.makeText(this, "YOU WON!\nHITS: " +hits +"   MISSES: " +misses, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "FINISHED!\nHITS: " +hits +"   " +
+                           "MISSES: " +misses, Toast.LENGTH_SHORT).show();
         }
     }
 }
